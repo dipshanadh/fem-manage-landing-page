a9da5f31f6e58ea240f2f9e0fa8edfa88557f2c5
Dipshan Adhikari <dipshanadh@gmail.com>
2023-10-15 21:44:14 +0545

    Start nav

---
+++ main.js
@@ -0,0 +1,10 @@
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
+
+navToggle.addEventListener("click", () => {
+	navToggle.setAttribute(
+		"aria-expanded",
+		!primaryNav.hasAttribute("data-visible")
+	);
+	primaryNav.toggleAttribute("data-visible");
+});
