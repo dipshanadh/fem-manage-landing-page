abc33b0ab59149b1096a5be61626440640cf5054
Dipshan Adhikari <dipshanadh@gmail.com>
2023-10-26 13:13:25 +0545

    Make carousel

--- main.js
+++ main.js
@@ -2,9 +2,24 @@ const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
 
 navToggle.addEventListener("click", () => {
-	navToggle.setAttribute(
-		"aria-expanded",
-		!primaryNav.hasAttribute("data-visible")
-	);
-	primaryNav.toggleAttribute("data-visible");
+  navToggle.setAttribute(
+    "aria-expanded",
+    !primaryNav.hasAttribute("data-visible")
+  );
+  primaryNav.toggleAttribute("data-visible");
+});
+
+const slider = new A11YSlider(document.querySelector(".slider"), {
+  adaptiveHeight: true,
+  dots: false,
+  arrows: false,
+  slidesToShow: 1,
+  responsive: {
+    480: {
+      slidesToShow: 2,
+    },
+    1024: {
+      slidesToShow: 3,
+    },
+  },
 });
